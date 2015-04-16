02c8b240d0f98d93f6e633b9a664e350d284a457
xiangshouding <fansekey@gmail.com>
2015-04-16 17:36:09 +0800

    del build proc

--- install.js
+++ install.js
@@ -2,13 +2,13 @@ try {
     require("./binding.js"); //try to load pre-build lib
 } catch (e) {
 	// build addon
-    var spawn = require('child_process').spawn,
-        gyp = spawn('node-gyp', ['rebuild']);
+    // var spawn = require('child_process').spawn,
+    //     gyp = spawn('node-gyp', ['rebuild']);
 
-    gyp.stdout.pipe(process.stdout);
-    gyp.stderr.pipe(process.stderr);
+    // gyp.stdout.pipe(process.stdout);
+    // gyp.stderr.pipe(process.stderr);
 
-    gyp.on('exit', function(code) {
-        process.exit(code);
-    });
+    // gyp.on('exit', function(code) {
+    //     process.exit(code);
+    // });
 }
